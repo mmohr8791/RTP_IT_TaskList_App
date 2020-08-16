fd03ffd2fece8026599223dbdb5ba73b50d00487
mike.mohr <mmohr.55987@gmail.com>
2020-08-16 11:29:50 -0500

    Modified index.js for main app js file name change

--- rtp_it_tasklist_app/src/index.js
+++ rtp_it_tasklist_app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ITTaskList from './components/IT_TaskList';
+import ITTaskList from './components/ITTaskList';
 
 ReactDOM.render(
   <React.StrictMode>
